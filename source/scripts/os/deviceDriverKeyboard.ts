f6262f18ae974b5b88f940ae5d1ddb273ada4c47
Garrett Oreilly <garrettoreilly@icloud.com>
2014-09-27 18:52:20 -0400

    Hard coded punctuation and symobls. !@#$

--- source/scripts/os/deviceDriverKeyboard.ts
+++ source/scripts/os/deviceDriverKeyboard.ts
@@ -32,7 +32,7 @@ module TSOS {
             var chr = "";
             // Check to see if we even want to deal with the key that was pressed.
             if (((keyCode >= 65) && (keyCode <= 90)) ||   // A..Z
-                ((keyCode >= 97) && (keyCode <= 123))) {  // a..z {
+                ((keyCode >= 97) && (keyCode <= 122))) {  // a..z {
                 // Determine the character we want to display.
                 // Assume it's lowercase...
                 chr = String.fromCharCode(keyCode + 32);
@@ -45,6 +45,94 @@ module TSOS {
             } else if (((keyCode >= 48) && (keyCode <= 57)) ||   // digits
                         (keyCode == 32)                     ||   // space
                         (keyCode == 13)) {                       // enter
+                if (isShifted) {
+                    if (keyCode == 48) {
+                        keyCode = 41;
+                    } else if (keyCode == 49) {
+                        keyCode = 33;
+                    } else if (keyCode == 50) {
+                        keyCode = 64;
+                    } else if (keyCode == 51) {
+                        keyCode = 35; 
+                    } else if (keyCode == 52) {
+                        keyCode = 36;
+                    } else if (keyCode == 53) {
+                        keyCode = 37;
+                    } else if (keyCode == 54) {
+                        keyCode = 94;
+                    } else if (keyCode == 55) {
+                        keyCode = 38;
+                    } else if (keyCode == 56) {
+                        keyCode = 42;
+                    } else if (keyCode == 57) {
+                        keyCode = 40;
+                    }
+                }
+                chr = String.fromCharCode(keyCode);
+                _KernelInputQueue.enqueue(chr);
+            } else if ((keyCode >= 186 && keyCode <= 192) ||
+                    (keyCode >= 219 && keyCode <= 222)) {
+                if (keyCode == 186){
+                    if (isShifted) {
+                        keyCode = 58;
+                    } else {
+                        keyCode = 59;
+                    }
+                } else if (keyCode == 187) {
+                    if (isShifted) {
+                        keyCode = 43;
+                    } else {
+                        keyCode = 61;
+                    }
+                } else if (keyCode == 188) {
+                    if (isShifted) {
+                        keyCode = 60;
+                    } else {
+                        keyCode == 44;
+                    }
+                } else if (keyCode == 189) {
+                    if (isShifted) {
+                        keyCode = 95;
+                    } else {
+                        keyCode = 45;
+                    }
+                } else if (keyCode == 190) {
+                    if (isShifted) {
+                        keyCode = 62;
+                    } else {
+                        keyCode = 46;
+                    }
+                } else if (keyCode == 191) {
+                    if (isShifted) {
+                        keyCode = 63;
+                    } else {
+                        keyCode = 47;
+                    }
+                } else if (keyCode == 192) {
+                    if (isShifted) {
+                        keyCode = 126;
+                    } else {
+                        keyCode = 96;
+                    }
+                } else if (keyCode == 219) {
+                    if (isShifted) {
+                        keyCode = 123;
+                    } else {
+                        keyCode = 91;
+                    }
+                } else if (keyCode = 220) {
+                    if (isShifted) {
+                        keyCode = 124;
+                    } else {
+                        keyCode = 92;
+                    }
+                } else if (keyCode = 221) {
+                    if (isShifted) {
+                        keyCode = 125;
+                    } else {
+                        keyCode = 93;
+                    }
+                }
                 chr = String.fromCharCode(keyCode);
                 _KernelInputQueue.enqueue(chr);
             }
